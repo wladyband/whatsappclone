ab99be6cbc9f97e1f7fca6e7829163de05676718
Wladimir Bandeira <tecnico100@gmail.com>
2025-01-19 17:38:25 -0300

    configuração básica

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    dependencies {
+        classpath ("com.google.gms:google-services:4.3.15")
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("com.google.dagger.hilt.android") version "2.42" apply false
+
+}
