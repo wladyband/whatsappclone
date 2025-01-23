ffe445481540fe2c5f007b9da7224c36ac609937
Wladimir Bandeira <tecnico100@gmail.com>
2025-01-23 13:36:17 -0300

    criando tela basica

---
+++ app/src/main/java/com/whatsappaphaclone/whatsappclone/LoginScreen.kt
@@ -0,0 +1,18 @@
+package com.whatsappaphaclone.whatsappclone
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+
+@Composable
+fun LoginScreen(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text = "Signup screen")
+    }
+}
---
+++ app/src/main/java/com/whatsappaphaclone/whatsappclone/ui/SignupScreen.kt
@@ -0,0 +1,19 @@
+package com.whatsappaphaclone.whatsappclone.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun SignupScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text = "Signup screen")
+    }
+
+
+}
